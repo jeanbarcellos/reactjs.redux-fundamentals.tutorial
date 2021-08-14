5f033edc45394c91631a6b364c046f57917f538b
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-14 17:46:49 -0300

    Buscando Todos de um Servidor

--- src/features/todos/todosSlice.js
+++ src/features/todos/todosSlice.js
@@ -1,3 +1,5 @@
+import { client } from '../../api/client'
+
 const initialState = []
 
 function nextTodoId(todos) {
@@ -7,6 +9,11 @@ function nextTodoId(todos) {
 
 export default function todosReducer(state = initialState, action) {
   switch (action.type) {
+    case 'todos/todosLoaded': {
+      // Replace the existing state entirely by returning the new value
+      return action.payload
+    }
+
     case 'todos/todoAdded': {
       // Can return just the new todos array - no extra object around it
       return [
@@ -57,4 +64,16 @@ export default function todosReducer(state = initialState, action) {
     default:
       return state
   }
+}
+
+export async function fetchTodos(dispatch, getState) {
+  const response = await client.get('/fakeApi/todos')
+
+  const stateBefore = getState()
+  console.log('Todos before dispatch: ', stateBefore.todos.length)
+
+  dispatch({ type: 'todos/todosLoaded', payload: response.todos })
+
+  const stateAfter = getState()
+  console.log('Todos after dispatch: ', stateAfter.todos.length)
 }
--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
+import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducer'
 
 const composedEnhancer = composeWithDevTools(
-  // Add whatever middleware you actually want to use here
-  applyMiddleware()
-  // other store enhancers if any
+  applyMiddleware(thunkMiddleware)
 )
+
 const store = createStore(rootReducer, composedEnhancer)
 export default store
