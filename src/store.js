2ca0246d6b2b78c862dfb2ebf24758f36b64a863
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-14 16:58:26 -0300

    Midlewares

--- src/store.js
+++ src/store.js
@@ -1,8 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import rootReducer from './reducer'
-import { print1, print2, print3 } from './exampleAddons/middleware'
+import { delayedMessageMiddleware } from './exampleAddons/middleware'
 
-const middlewareEnhancer = applyMiddleware(print1, print2, print3)
+const middlewareEnhancer = applyMiddleware(delayedMessageMiddleware)
 
 // Pass enhancer as the second arg, since there's no preloadedState
 const store = createStore(rootReducer, middlewareEnhancer)
