624d545881df298cfbab3340eb7b52190a7b8f2f
Jean Barcellos <jeanbarcellos@hotmail.com>
2021-08-14 17:20:41 -0300

    Passing the Store with Provider

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
-import App from './App'
-
 import './api/server'
 
+import App from './App'
 import store from './store'
-
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
